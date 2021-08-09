61ad224ee99408d25698aef98f856bad59f301fb
Daniela <dyepes@bostonprojectllc.com>
2021-08-09 15:45:30 -0500

    fixing eslint error with next pages 2

--- components/NavLink.js
+++ components/NavLink.js
@@ -1,8 +1,12 @@
-// import Link from "next/link";
+import Link from 'next/link';
+
 export default function NavLink({ linkLabel, linkUrl }) {
   return (
-    <a href={linkUrl} className="navegation__item">
-      <li>{linkLabel}</li>
-    </a>
+    <Link href={linkUrl}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a className="navegation__item">
+        <li>{linkLabel}</li>
+      </a>
+    </Link>
   );
 }
