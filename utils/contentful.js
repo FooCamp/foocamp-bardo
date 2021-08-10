ba70b630effba756166669d8de329776cbb78ab2
Orlando Guevara <orlandosgd96@gmail.com>
2021-08-10 10:43:20 -0400

    API adjustment to use multiple functions

--- utils/contentful.js
+++ utils/contentful.js
@@ -1,6 +1,12 @@
 const dataContentful = {};
-export default function headerContentful(data) {
+
+export function headerContentful(data) {
   dataContentful.logo = data.data.items[0].fields.components[0].fields.headerLogoLink;
   dataContentful.navLinks = data.data.items[0].fields.components[0].fields.headerNavLinks;
+  return dataContentful;
+}
+
+export function heroContentful(data) {
+  dataContentful.logo = data.data.items[0].fields.components[1].fields;
   return dataContentful;
 }
