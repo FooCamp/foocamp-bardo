c32e43e7dd92222dddfdfc0e9225c93f10de054d
Nelson Barrera <nbarrera@hugeinc.com>
2021-08-12 15:22:05 -0500

    Delete logs

--- utils/contentful.js
+++ utils/contentful.js
@@ -1,7 +1,6 @@
 const dataContentful = {};
 
 export function getContentfulHeaderData(data) {
-  console.log(data);
   dataContentful.logo = data.data.items[0].fields.components[0].fields.headerLogoLink;
   dataContentful.navLinks = data.data.items[0].fields.components[0].fields.headerNavLinks;
   return dataContentful;
