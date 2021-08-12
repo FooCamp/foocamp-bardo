ffc232d5715cc2a9dc978d96178cc2f1cec6be04
Daniela <dyepes@bostonprojectllc.com>
2021-08-12 08:21:57 -0500

    fixing the cursor and the index in the header

--- components/NavLink.js
+++ components/NavLink.js
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function NavLink({ linkLabel, linkUrl }) {
   return (
-    <Link href={linkUrl}>
-      <li className="navigation__item">
+    <li className="navigation__item">
+      <Link href={linkUrl}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a>{linkLabel}</a>
-      </li>
-    </Link>
+      </Link>
+    </li>
   );
 }
