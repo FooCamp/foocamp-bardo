417c8335f7376932322edd5b3660f05d882f8665
Nelson Barrera <nbarrera@hugeinc.com>
2021-08-25 16:46:27 -0500

    Add Methodology component Add role attribute to Image component

---
+++ components/Methodology/Methodology.js
@@ -0,0 +1,48 @@
+import { getContentfulMethodologyData } from '../../utils/contentful';
+import MethodologyTitle from '../Hero/HeroTitle';
+import MethodologyBody from '../Hero/HeroBody';
+import Image from '../Image';
+import Container from '../Container';
+
+export default function Methodology({ data }) {
+  const methodologyData = getContentfulMethodologyData(data);
+  const { sectionTitle, sectionDescription, sectionImages } = methodologyData;
+
+  return (
+    // <article id={identifier} className="methodology--child">
+    <section className="methodology">
+      <Container className="methodology__container">
+        <div className="methodology__container--child">
+          <MethodologyTitle
+            className="hero__title--child"
+            classNameWrapper="methodology__title-wrapper--child"
+          >
+            {sectionTitle}
+          </MethodologyTitle>
+        </div>
+        <div className="methodology__container--child">
+          <MethodologyBody
+            className="hero__body--child"
+            classNameWrapper="methodology__body-wrapper--child"
+          >
+            {sectionDescription}
+          </MethodologyBody>
+        </div>
+        <div className="methodology__container--child">
+          <div className="methodology__images__wrapper">
+            {sectionImages.map((component) => (
+              <Image
+                key={component.fields.title}
+                url={component.fields.file.url}
+                roleName="decorative"
+                className="methodology__image"
+                classNameWrapper="methodology__image__wrapper--child"
+              />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </section>
+    // </article>
+  );
+}
