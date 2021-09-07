5c60aad3f3a154bbda24fd7fab8197efc0f23739
nbarrerabs <68615447+nbarrerabs@users.noreply.github.com>
2021-09-07 09:57:29 -0500

    Add favicon to header (#34)
    Co-authored-by: Nelson Barrera <nbarrera@hugeinc.com>

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Navigation from './Navigation';
 import Footer from './Footer';
 
 export default function Layout({ data, children }) {
   return (
     <div className="layout">
+      <Head>
+        <link rel="shortcut icon" href="../favicon.png" />
+      </Head>
       <Navigation {...children} />
       <div className="page-content">{children}</div>
       <Footer data={data} />
