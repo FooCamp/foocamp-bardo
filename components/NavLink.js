1a7a915e9ba3ce9117f14263d10f2157c3a00bd3
Daniela <dyepes@bostonprojectllc.com>
2021-09-16 19:18:57 -0500

    Remove nav link slash

--- components/NavLink.js
+++ components/NavLink.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NavLink({ linkLabel, linkUrl }) {
   return (
     <li className="navigation__item">
-      <Link href={`/${linkUrl}`}>
+      <Link href={`${linkUrl}`}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a>{linkLabel}</a>
       </Link>
