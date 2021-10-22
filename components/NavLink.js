c0e27f5c2153ef074e100959de52916760a3449f
Adrian Priale <sinuhe-10@hotmail.com>
2021-10-22 14:20:59 -0500

    Link target on menu-profile and height of thumb in artwork

--- components/NavLink.js
+++ components/NavLink.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 
 export default function NavLink({ linkLabel, linkUrl }) {
+  const target = (linkUrl.length > 0 && linkUrl.substr(0, 1) !== '/') && '_blank';
   return (
     <li className="navigation__item">
       <Link href={linkUrl}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a>{linkLabel}</a>
+        <a target={target}>{linkLabel}</a>
       </Link>
     </li>
   );
